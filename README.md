aa6d061a638f93886c2b94d82dc0b9488913afdc
Tushar saini <tusharcsemitrc2020@gmail.com>
2023-10-29 11:06:25 +0530

    done
    ddone

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-# Request Header Parser Microservice
+# URL Shortener Microservice
 
-This is the boilerplate for the Request Header Parser Microservice project. Instructions for building your project can be found at https://www.freecodecamp.org/learn/apis-and-microservices/apis-and-microservices-projects/request-header-parser-microservice
+This is the boilerplate code for the URL Shortener Microservice project. Instructions for building your project can be found at https://www.freecodecamp.org/learn/back-end-development-and-apis/back-end-development-and-apis-projects/url-shortener-microservice.
--- index.js
+++ index.js
@@ -1,34 +1,55 @@
-// index.js
-// where your node app starts
-
-// init project
 require('dotenv').config();
-var express = require('express');
-var app = express();
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser')
+const app = express();
+const checkUrl = require('is-url')
+// Basic Configuration
+const port = process.env.PORT || 3000;
+let uniqueId = 1;
+const shortUrl = {}
 
-// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC
-var cors = require('cors');
-app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 204
 
-// http://expressjs.com/en/starter/static-files.html
-app.use(express.static('public'));
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use('/public', express.static(`${process.cwd()}/public`));
 
-// http://expressjs.com/en/starter/basic-routing.html
+
 app.get('/', function (req, res) {
-  res.sendFile(__dirname + '/views/index.html');
+  res.sendFile(process.cwd() + '/views/index.html');
 });
 
-// your first API endpoint...
-app.get('/api/whoami', (req, res) => {
-  console.log(req.socket.remoteAddress)
-  var ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress
-  res.json({
-    ipaddress: ip, language: req.headers['accept-language'], software: req.headers['user-agent']
+
+// Your first API endpoint
+app.get('/api/hello', function (req, res) {
+  res.json({ greeting: 'hello API' });
+});
+
+app.post('/api/shorturl', (req, res) => {
+
+  if (!checkUrl(req.body.url)) {
+    res.send({ error: 'invalid url' })
+    return;
+  }
+
+  let originalUrl = req.body.url;
+
+  shortUrl[uniqueId] = req.body.url
+  console.log(shortUrl)
+  res.send({
+    original_url: originalUrl, short_url: uniqueId
   })
-});
+  uniqueId++;
+})
 
-// listen for requests :)
-var listener = app.listen(process.env.PORT || 3000, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
+app.get('/api/shorturl/:id', (req, res) => {
+  const id = req.params.id
+  const url = shortUrl[id]
+  res.redirect(url)
+}
+
+)
+
+app.listen(port, function () {
+  console.log(`Listening on port ${port}`);
 });
